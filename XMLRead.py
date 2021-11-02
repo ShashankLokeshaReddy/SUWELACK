5bd0a6098cf3d3ebd8c88789dc56a56f9d17ca6d
Abdullah Akber <Ayybeeshafi@users.noreply.github.com>
2021-11-02 14:30:33 +0100

    Read XML Files

---
+++ XMLRead.py
@@ -0,0 +1,11 @@
+import xml.etree.ElementTree as ET
+mytree = ET.parse('static/XML/T904.xml')
+DataRoot = mytree.getroot()
+#for x in DataRoot[0]:
+#     print(x.tag, x.attrib)
+print("Number\t\tGroupNumber\t\tName")
+for tags in DataRoot.findall('T904_Kostenstellen'):
+    Number =tags.find('T904_NR').text
+    Name = tags.find('T904_Bez').text
+    GroupNumber = tags.find('T904_GruppeNr').text
+    print(Number +"\t\t",GroupNumber +"\t\t\t",Name)
