cc61ea4239f73ecdd30a047bcbd94d852c53749e
Abdullah <abdullah.akber@iem.fraunhofer.de>
2022-10-30 14:37:32 +0100

    DB Connection Refactor #21

--- dbconnection.py
+++ dbconnection.py
@@ -25,31 +25,42 @@ config.read("ksmain.ini")
 FirmaNumber = config.get("TerminalName")
 """
 
-# Arbeitplatzlist = pd.read_sql_query("Select * from  rtp.dbo.T905_ArbMasch WHERE T905_FirmaNr = 'TE' "
-#                                     "and T905_Inaktiv <> 1 order by T905_Sort,T905_Nr",connection)
-# pd.options.display.max_columns = 6
-# print(len(Arbeitplatzlist['T905_Bez']))
-
-Arbeitplatzlist= pd.read_sql_query("SELECT T905_Nr,T905_bez,T905_Bez2,T905_KstNr,T905_Art,T905_Typ,T905_Akkord,T905_Leist,T905_Freigabe,T905_ArbGrNr,T905_Img,T905_StartScreen,T905_Schleuse "
+def getArbeitplazlist(connection):
+    arbeitplatzlist = pd.read_sql_query("SELECT T905_Nr,T905_bez,T905_Bez2,T905_KstNr,T905_Art,T905_Typ,T905_Akkord,T905_Leist,T905_Freigabe,T905_ArbGrNr,T905_Img,T905_StartScreen,T905_Schleuse "
                                    "FROM rtp.dbo.G905_TermPlatz INNER JOIN rtp.dbo.T905_ArbMasch ON G905_Platz = T905_nr AND G905_FirmaNr = T905_FirmaNr "
                                    "WHERE G905_FirmaNr = 'TE' AND G905_Nr =? AND T905_Inaktiv <>1 "
                                    "ORDER BY G905_Lfdnr", connection, params=[terminalNumber])
+    return arbeitplatzlist
 
-# print(Arbeitplatzlist)
+
+
+def getPersonaldetails(connection):
+    personalname = pd.read_sql_query("SELECT T912_Nr,T910_Name,T910_Vorname FROM rtp.dbo.T910_Personalliste "
+                                     "INNER JOIN rtp.dbo.T912_PersCard ON T910_Nr = T912_PersNr", connection, coerce_float=False)
+    personalname['T912_Nr'] = personalname['T912_Nr'].astype("string")
+    personalname['VorNameName'] = personalname["T910_Vorname"] + ", " + personalname["T910_Name"]
+    return personalname
+
+
+Arbeitplatzlist = getArbeitplazlist(connection)
+personalname = getPersonaldetails(connection)
+print(Arbeitplatzlist)
+print(personalname)
+
+
+
+
+
+
 personallListe = pd.read_sql_query("SELECT T910_Nr,T910_Name,T910_Vorname FROM rtp.dbo.T910_Personalliste",
-                                   connection, coerce_float=False)
+                                       connection, coerce_float=False)
 personallCard = pd.read_sql_query("SELECT T912_Nr,T912_Bez,T912_PersNr FROM rtp.dbo.T912_PersCard",
-                                  connection, coerce_float=False)
-personalname = pd.read_sql_query("SELECT T912_Nr,T910_Name,T910_Vorname FROM rtp.dbo.T910_Personalliste INNER JOIN rtp.dbo.T912_PersCard ON T910_Nr = T912_PersNr", connection, coerce_float=False)
-# print(personalname)
-personalname['T912_Nr'] = personalname['T912_Nr'].astype("string")
-personalname['VorNameName'] = personalname["T910_Vorname"] + ", " + personalname["T910_Name"]
+                                      connection, coerce_float=False)
 # print(personalname)
 # print(personallCard)
 # user['T910_Name']
 # username = personallCard.loc[personallCard['T912_Nr'] == 1519]
 # print(username['T912_Bez'].values[0])
-
 # should be shown when gemeinkosten buttons
 # user['T910_Name']
 # GKdata = pd.read_sql_query("select top 10000 TA05_FA_Nr, TA05_ArtikelBez,TA06_BelegNr from KSAlias.dbo.TA05_FAK1 "
