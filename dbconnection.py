20881d1b1bd0db2bc7665969714e514753d88ef6
Tim Heuwinkel <tim.heuwinkel@iem.fraunhofer.de>
2022-11-01 22:46:26 +0100

    correct database select

--- dbconnection.py
+++ dbconnection.py
@@ -8,7 +8,7 @@ import configparser
 user = 'ksadmin'
 password = 'ksadmin'
 host = 'HOC-W10-212\SQL19'
-DB = 'ksmaster'
+DB = 'rtp'
 driver = 'SQL Server Native Client 11.0'
 DATABASE_CONNECTION = f'mssql://{user}:{password}@{host}/{DB}?driver={driver}'
 engine = create_engine(DATABASE_CONNECTION)
@@ -29,7 +29,7 @@ FirmaNumber = config.get("TerminalName")
 
 def getArbeitplazlist(connection):
     arbeitplatzlist = pd.read_sql_query("SELECT T905_Nr,T905_bez,T905_Bez2,T905_KstNr,T905_Art,T905_Typ,T905_Akkord,T905_Leist,T905_Freigabe,T905_ArbGrNr,T905_Img,T905_StartScreen,T905_Schleuse "
-                                   "FROM rtp.dbo.G905_TermPlatz INNER JOIN rtp.dbo.T905_ArbMasch ON G905_Platz = T905_nr AND G905_FirmaNr = T905_FirmaNr "
+                                   "FROM ksalias.dbo.G905_TermPlatz INNER JOIN ksalias.dbo.T905_ArbMasch ON G905_Platz = T905_nr AND G905_FirmaNr = T905_FirmaNr "
                                    "WHERE G905_FirmaNr = 'TE' AND G905_Nr =? AND T905_Inaktiv <>1 "
                                    "ORDER BY G905_Lfdnr", connection, params=[terminalNumber])
     return arbeitplatzlist
@@ -37,8 +37,8 @@ def getArbeitplazlist(connection):
 
 
 def getPersonaldetails(connection):
-    personalname = pd.read_sql_query("SELECT T912_Nr,T910_Name,T910_Vorname FROM rtp.dbo.T910_Personalliste "
-                                     "INNER JOIN rtp.dbo.T912_PersCard ON T910_Nr = T912_PersNr", connection, coerce_float=False)
+    personalname = pd.read_sql_query("SELECT T912_Nr,T910_Name,T910_Vorname FROM ksalias.dbo.T910_Personalliste "
+                                     "INNER JOIN ksalias.dbo.T912_PersCard ON T910_Nr = T912_PersNr", connection, coerce_float=False)
     personalname['T912_Nr'] = personalname['T912_Nr'].astype("string")
     personalname['VorNameName'] = personalname["T910_Vorname"] + ", " + personalname["T910_Name"]
     return personalname
@@ -60,19 +60,19 @@ def getStatustableItem(firmNo, persNo, dateTime,currentTime):
 Arbeitplatzlist = getArbeitplazlist(connection)
 personalname = getPersonaldetails(connection)
 #DATETIME - format: YYYY-MM-DD HH:MI:SS
-statustableitemlist = getStatustableItem("01",1035,"2021-02-05 16:18:15",currentTime=dateTimeObj)
+#statustableitemlist = getStatustableItem("01",1035,"2021-02-05 16:18:15",currentTime=dateTimeObj)
 print(Arbeitplatzlist)
 print(personalname)
-print(statustableitemlist)
+#print(statustableitemlist)
 
 
 
 
 
 
-personallListe = pd.read_sql_query("SELECT T910_Nr,T910_Name,T910_Vorname FROM rtp.dbo.T910_Personalliste",
+personallListe = pd.read_sql_query("SELECT T910_Nr,T910_Name,T910_Vorname FROM ksalias.dbo.T910_Personalliste",
                                        connection, coerce_float=False)
-personallCard = pd.read_sql_query("SELECT T912_Nr,T912_Bez,T912_PersNr FROM rtp.dbo.T912_PersCard",
+personallCard = pd.read_sql_query("SELECT T912_Nr,T912_Bez,T912_PersNr FROM ksalias.dbo.T912_PersCard",
                                       connection, coerce_float=False)
 # print(personalname)
 # print(personallCard)
